d83d882885daa5dfaa22b53fa052d832d9eb729a
John Grant <johnkgrant@gmail.com>
2014-01-31 09:26:01 -0500

    Initial Commit

---
+++ DynamicPlayground/JGAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAAppDelegate.h
+//  DynamicPlayground
+//
+//  Created by John Grant on 1/31/2014.
+//  Copyright (c) 2014 John Grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DynamicPlaygroundTests/DynamicPlaygroundTests.m
@@ -0,0 +1,34 @@
+//
+//  DynamicPlaygroundTests.m
+//  DynamicPlaygroundTests
+//
+//  Created by John Grant on 1/31/2014.
+//  Copyright (c) 2014 John Grant. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DynamicPlaygroundTests : XCTestCase
+
+@end
+
+@implementation DynamicPlaygroundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
